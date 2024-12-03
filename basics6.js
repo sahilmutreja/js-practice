34f3c10450b010f8d47e52361e947dbe6de03b2d
Sahil Mutreja <sahilmutreja@outlook.com>
2024-12-03 21:59:33 +0100

    updated object

--- basics6.js
+++ basics6.js
@@ -1,21 +1,25 @@
 // objects in javascript
 let person = {
-    firstName:"John",
-    lastName:"Doe",
-    age:34
+    firstName: "John",
+    fullName: function() {   // defining a function 
+        console.log(this.firstName + " " + this.lastName)
+    },
+    lastName: "Doe",
+    age: 34
 }
 
 // Accessing properties of object
 console.log(person.firstName)
 console.log(person['lastName'])
 console.log(person['age'])
+person.fullName()
 
 // Update values of the property in an object
 person.age = 20
 console.log("Updated age - " + person.age)
 
 // Add and delete a property in an object
-person.gender='male'
+person.gender = 'male'
 console.log(person) // printing the entire object
 delete person.gender
 console.log(person) // printing the entire object again
@@ -25,5 +29,6 @@ console.log("Gender exist in person - " + ('gender' in person))
 console.log("Age exist in person - " + ('age' in person))
 
 // iterate over each property in an object 
-for (const key in person) 
+for (const key in person)
     console.log(key + " = " + person[key])
+
